c6e67fb735662c15054a6e93c597830b601582c9
Leena West <leenawesterlund2@hotmail.com>
2026-04-22 15:33:25 +0300

    Add assignment-6: count animal occurrences using reduce

---
+++ assignment-6.js
@@ -0,0 +1,9 @@
+const animals = ['dog', 'cat', 'dog', 'bird', 'cat', 'dog'];
+
+const count = animals.reduce((acc, animal) => {
+
+  acc[animal] = (acc[animal] || 0) + 1;
+  return acc;
+}, {});
+
+console.log(count);
