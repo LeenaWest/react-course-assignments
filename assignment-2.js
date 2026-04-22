13cd74be8fbd43e1c30cdb532720ea99251ce4c0
Leena West <leenawesterlund2@hotmail.com>
2026-04-22 14:28:20 +0300

    Add assignment-2 JS file with object destructuring

---
+++ assignment-2.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Assignment 2</title>
+  </head>
+  <body>
+    <script>
+      const movie = { title: "Inception", year: 2010, director: "Nolan" };
+      const { title, year } = movie;
+      console.log(`The movie "${title}" was released in ${year}.`);
+      alert(`The movie "${title}" was released in ${year}.`);
+    </script>
+  </body>
+</html>
